eca51d544f38e9470c5afa6f14fca635b112cce1
flicknewb <theowenmorris@gmail.com>
2020-07-02 09:44:08 -0700

    changes for aws instance

--- src/gpt2_selfgen_tweet_bot.py
+++ src/gpt2_selfgen_tweet_bot.py
@@ -26,7 +26,7 @@ def generate_trending_tweet():
     print("generating topical tweets on subject: "+ topic)
     #update the text file with current tweets
     file_name = '../data/topic_tweets/'+topic+'.txt'
-    topical_tweets = get_topic_tweets(topic, 2500)
+    topical_tweets = get_topic_tweets(topic, 1000)
     t_tweet_string = " || ".join(topical_tweets)
     with open(file_name, 'w') as f:
         f.write(t_tweet_string)
@@ -62,6 +62,7 @@ def generate_trending_tweet():
                         nsamples=10, 
                         run_name=topic, 
                         prefix=topic)
+    print('done generating tweets... ')
     #return 1 tweet
     with open('../data/generated_tweets.txt','r') as f:
         texts = f.read().split('====================')
@@ -70,7 +71,11 @@ def generate_trending_tweet():
         tweet = text.split(' || ')[0]
         if len(tweet) > len(topic)+2:
             tweets.append(tweet)
-    return choice(tweets)
+    print("Potential tweets:\n"+ tweets)
+    tweet = choice(tweets)
+    if len(tweet) > 280:
+        tweet = tweet[:280]
+    return tweet
 
 def choose_and_clean_tweet(tweets):
     idx = randrange(0,len(tweets))
